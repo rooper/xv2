fa2ae4a92e6cc7558f30a44d3f00708725e3c7e4
epsilon <loganrooper@epsilon.local>
2013-05-02 09:53:21 -0700

    Lucas' first edition, and my prototype relay scripts

---
+++ SerialRelayControl/SerialRelayControl.ino
@@ -0,0 +1,33 @@
+//xv oceans relay module serial control
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  digitalWrite(0, LOW);
+  digitalWrite(1, LOW);
+  Serial.println("Ready");
+}
+
+
+void loop() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    
+    if (inChar == '1') {
+      digitalWrite(1, HIGH);
+      Serial.println("turning on 1");
+    }
+    if (inChar == '0') {
+      digitalWrite(0, HIGH);
+      Serial.println("turning on 0");
+    }
+    if (inChar == '2') {
+      digitalWrite(0, LOW);
+      digitalWrite(1, LOW);
+      Serial.println("clearing");
+  }
+}
+
+
---
+++ XV_TestScript/XV_TestScript_1.ino
@@ -0,0 +1,47 @@
+int pin = 11; //This is the pin that information is being read from.
+String inputString = ""; //
+boolean messageFinished = false;
+
+void setup() {
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (messageFinished) {
+    Serial.println(inputString);
+    inputString = "";
+    messageFinished = false;
+  }
+}
+
+void serialEvent(){
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    
+    if (inChar == '0') {
+      digitalWrite(pin, LOW);
+    }
+    if (inChar == '1') {
+      digitalWrite(pin, HIGH);
+    }
+    if (inChar == 'on') {
+      digitalWrite(pin, HIGH);
+    }
+    if (inChar == 'off') {
+      digitalWrite(pin, LOW);
+    }
+    if (inChar == 'status') {
+      boolean pinStatus = digitalRead(pin);
+      Serial.print(pinStatus, BIN);
+    }
+    if (inChar == '2') {
+      boolean pinStatus = digitalRead(pin);
+      Serial.print(pinStatus, BIN);
+    }
+    if (inChar == '\n') {
+      messageFinished = true;
+    }
+  }
+}
+
