8dc36b57a8b871a9b988d549acf5f8ce66d089ff
epsilon <loganrooper@epsilon.local>
2013-05-02 14:34:00 -0700

    for real

--- XV_TestScript/XV_TestScript_1/XV_TestScript_1.ino
+++ XV_TestScript/XV_TestScript_1/XV_TestScript_1.ino
@@ -18,30 +18,35 @@ void loop() {
 void serialEvent(){
   while (Serial.available()) {
     char inChar = (char)Serial.read();
+    inputString += (String)inChar;
     
-    if (inChar == '0') {
-      digitalWrite(pin, LOW);
-    }
-    if (inChar == '1') {
-      digitalWrite(pin, HIGH);
-    }
-    if (inChar == 'on') {
-      digitalWrite(pin, HIGH);
-    }
-    if (inChar == 'off') {
-      digitalWrite(pin, LOW);
-    }
-    if (inChar == 'status') {
-      boolean pinStatus = digitalRead(pin);
-      Serial.print(pinStatus, BIN);
-    }
-    if (inChar == '2') {
-      boolean pinStatus = digitalRead(pin);
-      Serial.print(pinStatus, BIN);
-    }
     if (inChar == '\n') {
       messageFinished = true;
-    }
+    } 
   }
+  //test
+  
+  //execute command based on charset
+    if (inputString == "0") {
+        digitalWrite(pin, LOW);
+      }
+    if (inputString == "1") {
+      digitalWrite(pin, HIGH);
+    }
+    if (inputString == "on") {
+      digitalWrite(pin, HIGH);
+    }
+    if (inputString == "off") {
+      digitalWrite(pin, LOW);
+    }
+    if (inputString == "status") {
+      boolean pinStatus = digitalRead(pin);
+      Serial.print(pinStatus, BIN);
+    }
+    if (inputString == "2") {
+      boolean pinStatus = digitalRead(pin);
+      Serial.print(pinStatus, BIN);
+    }
+  
 }
 
