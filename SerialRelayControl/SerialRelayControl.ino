309df45e7d6c335f4a87dc74c30e308060b5d968
epsilon <loganrooper@epsilon.local>
2013-05-03 12:19:32 -0700

    Unfinished and untested.

--- SerialRelayControl/SerialRelayControl.ino
+++ SerialRelayControl/SerialRelayControl.ino
@@ -1,14 +1,36 @@
-int pin = 11; //This is the pin that information is being read from.
+//XV OCEANS http://xv-oceans.com
+//SERIAL RELAY CONTROL
+
+//SETUP
+//This is the pin that information is being read from.
+int pin0 = 0;
+int pin1 = 1; 
+int pin2 = 2; 
+int pin3 = 3; 
+int pin4 = 4; 
+int pin5 = 5; 
+int pin6 = 6; 
+int pin7 = 7; 
 String inputString = ""; //
 boolean messageFinished = false;
 
 void setup() {
-  pinMode(pin, OUTPUT);
+  pinMode(pin0, OUTPUT);
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+  pinMode(pin6, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  
   Serial.begin(9600);
   digitalWrite(pin, LOW);
   Serial.println("Ready!");
 }
 
+
+//MAIN LOOP
 void loop() {
   if (messageFinished) {
     Serial.println(inputString);
