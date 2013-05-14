fce9c2500616bee84f1789c2019481c77d7b2979
Unknown Name <lucasargh@gmail.com>
2013-05-13 22:30:26 -0700

    Bugfix, read-only usable, but barely

--- python/ClientModuleProxyv2.py
+++ python/ClientModuleProxyv2.py
@@ -2,44 +2,39 @@ import serial
 import socket
 
 usbPath = raw_input('Please enter the path to the device (/dev/tty...): ')
+host = '10.0.1.3'
+port = 80
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((host, port))
+print("Debug: Bind complete")
+server.listen(1)
+serversock, serveraddr = server.accept()
+serverfile = serversock.makefile('rw', 0)
 
 def main():
     serverCatch()
 
 def serverCatch():
-    host = ''
-    port = 24632
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((host, port))
-    server.listen(1)
-    serversock, serveraddr = server.accept()
-    serverfile = serversock.makefile('rw', 0)
-    serverfile.write('HTTP/1.1 200 OK\n\n')
-    serverfile.write('<html><head><title>Welcome %s!</title></head>' %(str(serveraddr)))
-    serverfile.write('<body><br>%s' % serial.log.readline())
-    serverfile.write('</body></html>')
+    temp_line = serverfile.readline()
+    serverfile.write(temp_line)
     serialLoop()
 
 def serialLoop():
-	serial_log = open("log.txt", "w")
+	serial_log = serverfile
 	#Define clientInput and usbPath
 	clientInput = raw_input('> ')
 	#Open Serial Comm.
-	print("Debug clientInput: " + clientInput)
 	if clientInput == exit:
 		serial_log.close()
 		server.close()
 	ser = serial.Serial(usbPath, 9600)
-	print("Debug serial started")
 	#Write clientInput to serial
 	ser.write(clientInput)
 	#Print actual line
 	temp_serial = ser.readline()
-	print ("Debug temp_serial")
 	serial_log.write(temp_serial + '\n')
-	print("Debug temp_serial: " + temp_serial)
-	serialLoop()
+	main()
 
 if __name__ == '__main__':
   main()
