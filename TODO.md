4b890910bb74e4a62a63ea8bed7d7134d06517b6
Unknown Name <lucasargh@gmail.com>
2013-05-13 20:50:12 -0700

    Extremely buggy web functionality added
    Basically unusable, just a lot of foundation work. Bugfix coming soon,
    along with a bit of code cleanup. All the components we need I THINK are
    there, but they're calling each other at the wrong times, and there is a
    lot of unneeded code that's clogging things up a bit.

--- TODO.md
+++
@@ -1,7 +0,0 @@
-+Turn all pins on
-+Turn all pins off
-+Return status of all pins
-
-~Different method of user input
-
--"?Ready!" message in Arduino, +Ready message in Python.
--- python/ClientModuleProxyv2.py
+++ python/ClientModuleProxyv2.py
@@ -1,20 +1,45 @@
+import serial
+import socket
+
 usbPath = raw_input('Please enter the path to the device (/dev/tty...): ')
 
 def main():
-    #Import PySerial
-	import serial
-	from time import sleep
+    serverCatch()
 
-    #Define clientInput and usbPath
+def serverCatch():
+    host = ''
+    port = 24632
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+    server.listen(1)
+    serversock, serveraddr = server.accept()
+    serverfile = serversock.makefile('rw', 0)
+    serverfile.write('HTTP/1.1 200 OK\n\n')
+    serverfile.write('<html><head><title>Welcome %s!</title></head>' %(str(serveraddr)))
+    serverfile.write('<body><br>%s' % serial.log.readline())
+    serverfile.write('</body></html>')
+    serialLoop()
+
+def serialLoop():
+	serial_log = open("log.txt", "w")
+	#Define clientInput and usbPath
 	clientInput = raw_input('> ')
-    #Open Serial Comm.
+	#Open Serial Comm.
+	print("Debug clientInput: " + clientInput)
+	if clientInput == exit:
+		serial_log.close()
+		server.close()
 	ser = serial.Serial(usbPath, 9600)
-    	#Write clientInput to serial
+	print("Debug serial started")
+	#Write clientInput to serial
 	ser.write(clientInput)
-    	#Ignore first two lines that will show up (Ready! and Type 1:1;)
-    	#Print actual line
-    	print ser.readline()
-	main()
+	#Print actual line
+	temp_serial = ser.readline()
+	print ("Debug temp_serial")
+	serial_log.write(temp_serial + '\n')
+	print("Debug temp_serial: " + temp_serial)
+	serialLoop()
 
 if __name__ == '__main__':
   main()
