fec989659645799872544c933be736be731d1e8f
Unknown Name <lucasargh@gmail.com>
2013-05-14 12:39:30 -0700

    Bugfix, still singleuse
    Now you can do more then 8 commands before it crashes.
    
    Refreshing the page will display all the responses, but it will also
    break the program after 2 more commands. Probably need to redefine the
    socket after they refresh, not sure about that though.

--- python/ClientModuleProxyv2.py
+++ python/ClientModuleProxyv2.py
@@ -2,8 +2,8 @@ import serial
 import socket
 
 usbPath = raw_input('Please enter the path to the device (/dev/tty...): ')
-host = '10.0.1.3'
-port = 80
+host = ''
+port = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((host, port))
@@ -13,27 +13,14 @@ serversock, serveraddr = server.accept()
 serverfile = serversock.makefile('rw', 0)
 
 def main():
-    serverCatch()
+    serverWrite()
 
-def serverCatch():
-    temp_line = serverfile.readline()
-    serverfile.write(temp_line)
-    serialLoop()
-
-def serialLoop():
-	serial_log = serverfile
-	#Define clientInput and usbPath
+def serverWrite():
+	server.listen(1)
 	clientInput = raw_input('> ')
-	#Open Serial Comm.
-	if clientInput == exit:
-		serial_log.close()
-		server.close()
 	ser = serial.Serial(usbPath, 9600)
-	#Write clientInput to serial
 	ser.write(clientInput)
-	#Print actual line
-	temp_serial = ser.readline()
-	serial_log.write(temp_serial + '\n')
+	serverfile.write(ser.readline())
 	main()
 
 if __name__ == '__main__':
