fc0b702a79b2412c56adda137f29507b7192094f
Unknown Name <lucasargh@gmail.com>
2013-05-16 17:14:27 -0700

    .txt --> .html bugfix/comments soon.

--- python/ClientModuleProxyv3.py
+++ python/ClientModuleProxyv3.py
@@ -1,14 +1,17 @@
 import serial
+import time
 
 usbPath = raw_input('Please enter the path to the device: ')
 def main():
 	serialWrite()
 def serialWrite():
-	clientInput = raw_input('> ')
+	#clientInput = raw_input('> ')
 	ser = serial.Serial(usbPath, 9600)
-	ser.write(clientInput)
-	logfile = open('log.txt', 'a+', 0)
-	logfile.write(ser.readline())
+	#ser.write(clientInput)
+	logfile = open('log.html', 'a+', 0)
+	logfile.write('<html><meta http-equiv="refresh" content ="5"></html>')
+	logfile.write('<p>' + ser.readline())
+	#logfile.write(ser.readline())
 	logfile.close()
 	main()
 
