82c2d1475068fe6136bda52ccff5998b15a99bcd
Unknown Name <lucasargh@gmail.com>
2013-05-16 19:30:49 -0700

    Clear log after 10 input, backup all logs.

--- python/ClientModuleProxyv3.py
+++ python/ClientModuleProxyv3.py
@@ -5,14 +5,24 @@ usbPath = raw_input('Please enter the path to the device: ')
 def main():
 	serialWrite()
 def serialWrite():
-	#clientInput = raw_input('> ')
+	i = 0
 	ser = serial.Serial(usbPath, 9600)
-	#ser.write(clientInput)
-	logfile = open('log.html', 'a+', 0)
-	logfile.write('<html><meta http-equiv="refresh" content ="5"></html>')
-	logfile.write('<p>' + ser.readline())
-	#logfile.write(ser.readline())
+	while i < 10:
+		clientInput = raw_input('> ')
+		ser.write(clientInput)
+		logfile = open('log.html', 'a+', 0)
+		logfile.write('<p>' + ser.readline())
+		i = i + 1
+		print ('Reached end of file')
 	logfile.close()
+	logfile = open('log.html', 'w+', 0)
+	backupfile = open('backup.txt', 'a+', 0)
+	for line in logfile.readline():
+		print line
+		backupfile.write(line)
+	logfile.write('<html><meta http-equiv="refresh" content ="5"></html>')
+	logfile.close()
+	backupfile.close()
 	main()
 
 if __name__ == '__main__':
