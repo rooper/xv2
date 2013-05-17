8a13a6c456660b0afb50c8a0e436a273db4505b7
Unknown Name <lucasargh@gmail.com>
2013-05-17 13:49:58 -0700

    Run.sh improvements

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /root/Desktop/xv2/python
+echo "Enter the path from / to the xv2 folder. E.G. /root/Desktop/xv2"
+read directory
+cd $directory/python
 nohup python -m SimpleHTTPServer &
 python ClientModuleProxyv3.py
